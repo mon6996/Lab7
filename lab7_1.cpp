fd252b9dd2a8a91fba85b358c26c0b5c2dd99ef0
mon6996 <thanaporn_chan@cmu.ac.th>
2021-11-30 15:00:16 +0700

    Lab7_1 complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,39 +3,58 @@
 
 using namespace std;
 
-string func1(string x){
+string func1(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while(i < L)
+	{
 		y += x[L-i-1];
 		i++;
 	}
 	return y;
 }
 
-string func2(string x){
+string func2(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while(i < L)
+	{
 		y += toupper(x[i]);
 		i++;
 	}
 	return y;	
 }
 
-string func3(string x){
+string func3(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while(i < L)
+	{
 		y += tolower(x[i]);
 		i++;
 	}
 	return y;	
 }
 
-int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+int main()
+{
+	string text;
+    cout << "Input text: ";
+	cin >> text;
+    cout << "Reversed text: ";
+	cout << func1(text) << endl;
+    cout << "Palindrome: ";
+	if(func1(func2(text)) == func2(text))
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
+	
     return 0;
 }
